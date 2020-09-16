8058b423126eda12fee20094e2e89eda6ad50f1f
Jon Erik D Suero <jonerikdsuero@gmail.com>
2020-09-16 09:25:19 -0400

    output more vars

--- modules/ecs_service_fargate/outputs.tf
+++ modules/ecs_service_fargate/outputs.tf
@@ -6,7 +6,15 @@ output "task_arn" {
   value = aws_ecs_task_definition.ecs_task.arn
 }
 
+output "my_ecs_service" {
+  value = aws_ecs_service.ecs_service
+}
+
+
+output "my_ecs_task" {
+  value = aws_ecs_task_definition.ecs_task
+}
+
 output "iam_id" {
   value = aws_iam_role.ecs_taskrole.id
 }
-
